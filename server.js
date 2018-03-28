3e26bec5dc35b8f9d0c46c7a7bb390a0f133778a
Janosch Zbick <janosch.zbick@gmail.com>
2018-03-28 23:19:29 +0200

    ...

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var http = require('http');
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || '3000'),
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.set('port', port);
 
